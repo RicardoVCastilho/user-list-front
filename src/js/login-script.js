b114a066245fe099c5b5e94061e51a586f796526
Ricardo Vitor <ricardovdev@hotmail.com>
2024-12-11 07:09:40 -0300

    adding logic and API connection

---
+++ src/js/login-script.js
@@ -0,0 +1,39 @@
+document.getElementById('login-form').addEventListener('submit', function (event) {
+    event.preventDefault();
+
+    const email = document.getElementById('email').value;
+    const password = document.getElementById('password').value;
+
+    if (!email || !password) {
+        alert('É necessário preencher email e senha!');
+        return;
+    }
+
+    const loginData = {
+        email: email,
+        password: password
+    };
+
+    fetch('https://api-login-z19r.onrender.com/auth/login', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(loginData)
+    })
+    .then(response => {
+        if (!response.ok) {
+            return response.json().then(data => {
+                throw new Error(data.msg);
+            });
+        }
+        return response.json();
+    })
+    .then(data => {
+        window.location.href = '/src/html/user-dashboard.html'
+    })
+    .catch(error => {
+        console.error('Erro:', error);
+        alert('Erro: ' + error.message); // Exibe a mensagem de erro
+    });
+});
