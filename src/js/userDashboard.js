188edb66e6af8eaeab007393ada11f8d25f4b06b
Ricardo Vitor <ricardovdev@hotmail.com>
2024-12-17 16:44:52 -0300

    uploading final styles prototype includin responsive

--- src/js/userDashboard.js
+++ src/js/userDashboard.js
@@ -1,4 +1,11 @@
-document.getElementById("hamburgerMenu").addEventListener("click", function () {
-    document.getElementById("navBar").classList.toggle("open");
-});
+// Função do menu responsivo
+function showMenu() {
+    const hamburgerMenu = document.querySelector('.hamburguer-menu');
+    const icon = document.querySelector('.icon');
 
+    // Alterna a classe 'open' e altera o ícone correspondente
+    const isOpen = hamburgerMenu.classList.toggle('open');
+    icon.src = isOpen 
+        ? "./src/images/close_white_36dp.svg" 
+        : "./src/images/menu_white_36dp.svg";
+}
